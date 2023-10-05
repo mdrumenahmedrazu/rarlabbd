e66b37350729e9ec002cd32c7091476529f81d1d
MD. RUMEN AHMED RAZU <69864123+mdrumenahmedrazu@users.noreply.github.com>
2023-10-05 11:54:09 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,86 @@
+<!doctype html>
+ <html class="no-js" lang="">
+
+	<head>
+	  <meta charset="utf-8">
+	  <meta http-equiv="x-ua-compatible" content="ie=edge">
+	  <title>Marktoo NFT Marketplace Template</title>
+	  <meta name="description" content="">
+	  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	  <link rel="manifest" href="site.webmanifest">
+	  <link rel="shortcut icon" type="x-icon" href="favicon.ico">
+	 
+	  <link rel="stylesheet" href="css/font-awesome.min.css">
+	  <link rel="stylesheet" href="css/owl.carousel.min.css">
+	  <link rel="stylesheet" href="css/animate.min.css">
+	  <link rel="stylesheet" href="css/line-awesome.css">	  
+	  <link rel="stylesheet" href="css/bootstrap.min.css">
+	  <link rel="stylesheet" href="css/jquery.fancybox.css">
+	  <link rel="stylesheet" href="css/magnific-popup.css">
+	  <link rel="stylesheet" href="css/slicknav.css">
+	  <link rel="stylesheet" href="css/normalize.css">
+	  <link rel="stylesheet" href="css/style.css">
+	  <link rel="stylesheet" href="css/responsive.css">
+	  <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,500i,600,700,800&amp;display=swap" rel="stylesheet">
+	  
+	</head>
+
+	<body>
+	  <!--[if lte IE 9]>
+		<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+	  <![endif]-->
+	  
+		<div class="hola">
+			<div class="Shape_5 Top_Border"></div>
+			<h2>Loading</h2>
+		</div>
+	  	
+		    <?php
+    include "header_main.php";
+    ?>
+	   
+	   <section>
+			<div class="slider-area">
+				<div class="slider-active-warp">
+					<div class="slider-wrapper demo-2 vh d-flex" style="background-image:url(img/slider/bg4.jpg)">
+						<div class="container">
+							<div class="col-md-12">
+								<div class="slider-content text-center mt-4">									
+									<h1>Welcome to JCPSCAA</h1>
+									<p>Delivering beautiful digital products. Digital marketplace for Buy, 
+									sell, and discover exclusive digital assets.</p>
+									<a href="explore-2.html" class="btn btn-color btn-bg-2 btn-space-2 btn-hover-2 btn-border-2 margin-right">Explore</a>
+									<a href="create.html" class="btn btn-color-2 btn-bg btn-space btn-hover">Create</a>
+								</div>
+							</div>								
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		
+		    <?php
+    include "footer_main.php";
+    ?>
+		
+
+		<script src="js/vendor/modernizr-3.6.0.min.js"></script>
+		<script src="js/vendor/jquery-3.5.1.min.js"></script>		  	  
+		<script src="js/jquery.scrollUp.min.js"></script>			
+		<script src="js/waypoints.min.js"></script>		 		 
+		<script src="js/bootstrap.min.js"></script> 	  
+		<script src="js/jquery.sticky-sidebar.min.js"></script> 	  
+		<script src="js/jquery.slicknav.min.js"></script>
+		<script src="js/smooth-scrollbar.js"></script>
+		<script src="js/fancyBox v2.1.5.js"></script>		
+		<script src="js/owl.carousel.min.js"></script>			
+		<script src="js/main.js"></script>
+		<script src="js/isotope.pkgd.min.js"></script>
+		<script src="js/imagesloaded.pkgd.min.js"></script>
+		<script src="js/plugins.js"></script>
+		  
+	</body>
+
+</html>
+
